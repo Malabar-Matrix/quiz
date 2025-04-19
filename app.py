a8ef2ed0dd22fdf44579207ee09d26e9ea3595dd
ERROR-SIDDH <57867831+ERROR-SIDDH@users.noreply.github.com>
2025-04-19 17:47:24 +0530

    Refactor Vercel configuration and comment out app.run in main

--- app.py
+++ app.py
@@ -123,5 +123,5 @@ def submit_quiz():
 
 # ------------ MAIN ------------
 
-if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0", port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True,host="0.0.0.0", port=5000)
